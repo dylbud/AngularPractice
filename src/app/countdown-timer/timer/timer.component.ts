65aa1e7c91313bb06763cb08b07901e51b30a6d1
Dylan J. Bragg <dylbudbragg@gmail.com>
2020-04-02 09:16:30 -0700

    modified timer component

--- src/app/countdown-timer/timer/timer.component.ts
+++ src/app/countdown-timer/timer/timer.component.ts
@@ -8,11 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  years: number = 2;
+  months: number = 5;
+  days: number = 24;
+  hours: number = 8;
+  minutes: number = 56;
+  seconds: number = 2;
+
+  @Input()
+  eventNameInput: string;
+
   constructor() { }
 
-  @Input() eventNameInput: string;
-  @Input() eventDateInput: Date;
-  @Input() eventTimeInput: Date;
+
 
 
 
