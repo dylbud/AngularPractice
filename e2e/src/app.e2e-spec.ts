7a3ad8e9d18324afe523f278f20c0eefbf4378ca
Dylan J. Bragg <dylbudbragg@gmail.com>
2020-04-06 09:56:22 -0700

    Changed name of project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('DarkTruth app is running!');
+    expect(page.getTitleText()).toEqual('AngularPractice app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DarkTruth'`, () => {
+  it(`should have as title 'AngularPractice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DarkTruth');
+    expect(app.title).toEqual('AngularPractice');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DarkTruth app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularPractice app is running!');
   });
 });
