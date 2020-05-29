2ce8462bc134250b7895fb484109c750608c25af
Dylan J. Bragg <dylbudbragg@gmail.com>
2020-05-28 22:05:21 -0700

    Notes app working

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,15 +1,89 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+export class Note {
+  subject: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.css']
+  styleUrls: ['./notes.component.css'],
 })
 export class NotesComponent implements OnInit {
+  form: FormGroup;
+  subject = new FormControl('', Validators.required);
+  showEditButton = false;
+  body = new FormControl('', Validators.required);
+  editingIndex: number;
 
-  constructor() { }
+  notes: Note[];
 
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      subject: this.subject,
+      body: this.body,
+    });
   }
 
+  onSubmit() {
+    const newNote: Note = {
+      subject: this.subject.value,
+      body: this.body.value,
+    };
+    this.notes.push(newNote);
+    this.setStorage();
+    this.clearForm();
+  }
+
+  ngOnInit() {
+    if (window.localStorage.getItem('notes')) {
+      this.notes = JSON.parse(window.localStorage.getItem('notes'));
+    } else {
+      this.notes = [];
+    }
+  }
+
+  setStorage() {
+    window.localStorage.setItem('notes', JSON.stringify(this.notes));
+    this.showEditButton = false;
+  }
+
+  clearAllNotes() {
+    if (confirm('Are you sure?')) {
+      this.notes = [];
+      this.setStorage();
+      this.clearForm();
+    }
+  }
+
+  delete(index: number) {
+    this.notes.splice(index, 1);
+    this.setStorage();
+  }
+
+  selectNoteToEdit(index: number) {
+    this.editingIndex = index;
+    this.subject.setValue(this.notes[index].subject);
+    this.body.setValue(this.notes[index].body);
+    this.showEditButton = true;
+  }
+
+  clearForm() {
+    this.form.reset();
+    this.showEditButton = false;
+  }
+
+  edit() {
+    this.notes[this.editingIndex].subject = this.subject.value;
+    this.notes[this.editingIndex].body = this.body.value;
+    this.setStorage();
+    this.showEditButton = false;
+  }
 }
