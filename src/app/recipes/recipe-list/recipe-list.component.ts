dabb68ba008c81b7989da84ca30285234c82e98b
Dylan J. Bragg <dylbudbragg@gmail.com>
2020-06-02 21:36:26 -0700

    Finalized recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { Observable } from 'rxjs';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+export class IngredientMeasure {
+  measure: string;
+  ingredient: string;
+}
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,20 +18,46 @@ export class RecipeListComponent implements OnInit {
   data: object;
   form: FormGroup;
   searchTerm: FormControl = new FormControl();
+  modalRef: BsModalRef;
+  mealTitle: string;
+  ingredients: IngredientMeasure[];
+  instructions: string;
+  mealImage: string;
 
-  constructor(fb: FormBuilder, private service: RecipeService) {
+  constructor(
+    fb: FormBuilder,
+    private service: RecipeService,
+    private modalService: BsModalService
+  ) {
     this.form = fb.group({
       searchTerm: this.searchTerm,
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.ingredients = new Array<IngredientMeasure>();
+  }
 
   onSubmit() {
     this.service.searchRecipes(this.searchTerm.value).subscribe((data) => {
-      console.log(data);
       this.data = data;
-      // console.log(this.data);
     });
+  }
+
+  openModal(template: TemplateRef<any>, meal: any) {
+    this.mealTitle = meal.strMeal;
+    this.mealImage = meal.strMealThumb;
+    this.instructions = meal.strInstructions;
+    this.ingredients = new Array<IngredientMeasure>();
+    for (let i = 1; i <= 20; i++) {
+      const ingMeas: IngredientMeasure = new IngredientMeasure();
+      ingMeas.ingredient = meal['strIngredient' + i];
+      ingMeas.measure = meal['strMeasure' + i];
+      if (ingMeas.ingredient !== '' && ingMeas.measure !== '') {
+        this.ingredients.push(ingMeas);
+      }
+    }
+    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
+    console.log(this.ingredients);
   }
 }
